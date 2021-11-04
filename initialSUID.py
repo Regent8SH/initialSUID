59770fc814d552b9ec15c8a6910298e868afa058
Regen Peterson <regent8sh@gmail.com>
2021-11-03 22:10:24 -0400

    initialSUID.py
    Python script to check for SUID binaries and remove standard entries for easier enumeration. Yeah linenum probably already does this... oh well.

---
+++ initialSUID.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+'''
+
+Removes any exteremly common SUID binaries from the SUID search. Will update as necessary.
+
+Usage:
+
+./suickSUID.py
+
+'''
+
+import sys, os
+
+std = ["chsh", "sudo", "newgrp", "ntfs", "kismet", "fusermount3", "vmware", "chfn", "passwd", "su", "pkexec", "gpasswd", "mount", "umount", "pppd"]
+
+outfile = "/tmp/qsuid.tmp"
+
+def main():
+	os.system(r"find / -type f -perm -u=s -exec ls -ldb {} \; 2>/dev/null >> /tmp/qsuid.tmp")
+
+	with open(outfile) as f:
+		scan = f.readlines()
+
+		for line in scan:
+			if not any(binary in line for binary in std):
+				print(line)
+
+	if os.path.exists(outfile):
+		os.remove(outfile)
+
+
+if __name__ == "__main__":
+   main()
